079f69db044a761df26ae5ba981ad277394f46d7
MasterDan <MihaylovDS@gmail.com>
2022-05-05 21:25:32 +0300

    still draft

--- core/src/directives/builtin/templateStringDirective.ts
+++ core/src/directives/builtin/templateStringDirective.ts
@@ -2,7 +2,10 @@ import { Ref } from 'core/src/scope/ref';
 import { RexNode } from 'core/src/vdom/rexNode';
 import { combineLatest, first, map, switchMap } from 'rxjs';
 import { Directive } from '../directive';
-import { getKeysToInsert } from '../stringParser/stringParser';
+import {
+  getKeysToInsert,
+  parseTemplateString,
+} from '../stringParser/stringParser';
 
 export class TemplateStringDirective extends Directive {
   name = '__template_String__';
@@ -18,9 +21,23 @@ export class TemplateStringDirective extends Directive {
         this.resolveReactive<Ref<string>>(key).pipe(
           switchMap((v) => v),
           map((v) => (v == null ? '' : v)),
+          map((value) => ({
+            key,
+            value,
+          })),
         ),
       );
-      combineLatest(resolved).pipe(first()).subscribe;
+      combineLatest(resolved)
+        .pipe(first())
+        .subscribe((arr: { key: string; value: string }[]) => {
+          const acc: Record<string, string> = {};
+          for (const pair of arr) {
+            acc[pair.key] = pair.value;
+          }
+          node.children$.next(
+            parseTemplateString(node.children$.value as string, acc),
+          );
+        });
     }
     return node;
   }

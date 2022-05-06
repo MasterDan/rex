d679fed832b8a67132addc789df5ca81273c16b3
MasterDan <MihaylovDS@gmail.com>
2022-05-06 16:31:14 +0300

    fixed template pareser directive

--- core/src/directives/builtin/templateStringDirective.ts
+++ core/src/directives/builtin/templateStringDirective.ts
@@ -1,6 +1,14 @@
 import { Ref } from 'core/src/scope/ref';
 import { RexNode } from 'core/src/vdom/rexNode';
-import { combineLatest, first, map, switchMap } from 'rxjs';
+import {
+  combineLatest,
+  filter,
+  first,
+  forkJoin,
+  map,
+  of,
+  switchMap,
+} from 'rxjs';
 import { Directive } from '../directive';
 import {
   getKeysToInsert,
@@ -15,30 +23,57 @@ export class TemplateStringDirective extends Directive {
   }
 
   override init(node: RexNode): RexNode | RexNode[] {
-    if (this.childIndex == null && typeof node.children$.value === 'string') {
-      const keys = getKeysToInsert(node.children$.value);
-      const resolved = keys.map((key) =>
-        this.resolveReactive<Ref<string>>(key).pipe(
-          switchMap((v) => v),
-          map((v) => (v == null ? '' : v)),
-          map((value) => ({
-            key,
-            value,
-          })),
+    node.children$
+      .pipe(
+        map((val) => {
+          if (this.childIndex == null && typeof val === 'string') {
+            return val;
+          } else if (this.childIndex != null && Array.isArray(val)) {
+            return val[this.childIndex] ?? null;
+          }
+        }),
+        filter(
+          (strToRepl): strToRepl is string =>
+            strToRepl != null && typeof strToRepl === 'string',
         ),
-      );
-      combineLatest(resolved)
-        .pipe(first())
-        .subscribe((arr: { key: string; value: string }[]) => {
+        switchMap((strToRepl) => {
+          const keys = getKeysToInsert(strToRepl);
+          const resolved = keys.map((key) =>
+            this.resolveReactive<Ref<string>>(key).pipe(
+              switchMap((v) => v),
+              map((v) => (v == null ? '' : v)),
+              map((value) => ({
+                key,
+                value,
+              })),
+            ),
+          );
+          return forkJoin({
+            template: of(strToRepl),
+            pairs: combineLatest(resolved).pipe(first()),
+          });
+        }),
+        map((arg) => {
           const acc: Record<string, string> = {};
-          for (const pair of arr) {
+          for (const pair of arg.pairs) {
             acc[pair.key] = pair.value;
           }
-          node.children$.next(
-            parseTemplateString(node.children$.value as string, acc),
-          );
-        });
-    }
+          return {
+            template: arg.template,
+            state: acc,
+          };
+        }),
+      )
+      .subscribe((arg) => {
+        const templateResult = parseTemplateString(arg.template, arg.state);
+        if (this.childIndex != null) {
+          const valueToModify = node.children$.value as string[];
+          valueToModify[this.childIndex] = templateResult;
+          node.children$.next(valueToModify);
+        } else {
+          node.children$.next(templateResult);
+        }
+      });
     return node;
   }
 }

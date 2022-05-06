3e7269e904feec9000f349a598272d6898323c50
MasterDan <MihaylovDS@gmail.com>
2022-05-06 17:59:55 +0300

    component and prop draft

--- core/src/component/component.ts
+++ core/src/component/component.ts
@@ -1,1 +1,12 @@
-export class Component {}
+import { Ref } from '../scope/ref';
+
+export interface IComponentConstructorArgs {
+  setup(): Record<string, Ref>;
+}
+
+export abstract class Component {
+  constructor(arg: IComponentConstructorArgs) {
+    console.log(arg);
+    throw new Error('Not Implemented');
+  }
+}

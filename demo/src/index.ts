a33303215408b54f8c8138921e529204f0e2f03d
MasterDan <MihaylovDS@gmail.com>
2022-05-18 20:23:21 +0300

    first rexar component

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -1,1 +1,17 @@
+import { Component, createApp, Ref, RexNode } from 'rexar';
 
+const root = new Component({
+  render: new RexNode(
+    'div',
+    null,
+    'Hello, {{ word }}. This is first rexar component',
+  ),
+  setup() {
+    const word = new Ref('Danny');
+    return {
+      word,
+    };
+  },
+});
+
+createApp(root).mount('#app');

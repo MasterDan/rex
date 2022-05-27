5181988647e85a02e82d225a3d4845cf412ffe10
MasterDan <MihaylovDS@gmail.com>
2022-05-27 21:08:41 +0300

    alt syntax fails

--- core/src/directives/builtin/bindDirective.spec.ts
+++ core/src/directives/builtin/bindDirective.spec.ts
@@ -27,4 +27,26 @@ describe('bind attribute directive', () => {
       /<div id="rexApp"><div id="World" --rex--anchor=".*"><\/div><\/div>/gm,
     );
   });
+  test('bind id alt', () => {
+    const word = new Ref('Danny');
+    const rootComponent = new Component({
+      render: new RexNode('div', {
+        'rex-bind:id': 'word',
+      }),
+      setup() {
+        return {
+          word,
+        };
+      },
+    });
+    const jsDom = new JsDomPlugin('<div id="rexApp" ></div>');
+    new RexApp(rootComponent).extend(jsDom).mount('#rexApp');
+    expect(jsDom.dom.window.document.body.innerHTML).toMatch(
+      /<div id="rexApp"><div id="Danny" --rex--anchor=".*"><\/div><\/div>/gm,
+    );
+    word.next('World');
+    expect(jsDom.dom.window.document.body.innerHTML).toMatch(
+      /<div id="rexApp"><div id="World" --rex--anchor=".*"><\/div><\/div>/gm,
+    );
+  });
 });

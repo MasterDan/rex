40d11331119bab4222908c0c4c28c901567783da
MasterDan <MihaylovDS@gmail.com>
2022-06-01 22:19:35 +0300

    test failing

--- core/src/domPrototype/rexNode.spec.ts
+++ core/src/domPrototype/rexNode.spec.ts
@@ -1,3 +1,6 @@
+import { JSDOM } from 'jsdom';
+import { documentKey } from '../di/constants';
+import { DiContainer } from '../di/diContainer';
 import { RexNode } from './rexNode';
 
 describe('rexNode', () => {
@@ -66,5 +69,28 @@ describe('rexNode', () => {
     const rexNode = new RexNode('div', { class: 'foo-bar' }, ['lorem ipsum']);
     const cloned = rexNode.clone();
     expect(cloned.compare(rexNode)).toBe(true);
+  });
+  test('insert many', () => {
+    const jsd = new JSDOM('<div id="rexApp" ></div>');
+    const di = new DiContainer();
+    di.register(jsd.window.document, documentKey);
+    const rexNode = new RexNode('div', { class: 'foo-bar' }, [
+      new RexNode('div', { class: 'foo-baz' }),
+      new RexNode('div', { class: 'foo-baz' }),
+    ]);
+    rexNode.setContainer(di);
+    const sub = jest.fn((fragment: DocumentFragment) => {
+      const root = jsd.window.document.querySelector('#rexApp');
+      expect(root).not.toBe(null);
+      expect(root).not.toBe(undefined);
+      if (root) {
+        root.appendChild(fragment);
+        expect(jsd.window.document.body.innerHTML).toBe(
+          '<div id="rexApp" ><div class="foo-bar"><div class="foo-baz"></div><div class="foo-baz"></div></div></div>',
+        );
+      }
+    });
+    rexNode.render().subscribe(sub);
+    expect(sub).toBeCalled();
   });
 });

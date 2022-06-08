19b23c2c1a914302de832fc19da614df41919c2a
MasterDan <MihaylovDS@gmail.com>
2022-06-08 20:10:18 +0300

    preparing new test

--- core/src/directives/builtin/ifDirective.spec.ts
+++ core/src/directives/builtin/ifDirective.spec.ts
@@ -1,5 +1,26 @@
+import { RexNode } from '../../domPrototype/rexNode';
+import { RexApp } from '../../app/app';
+import { JsDomPlugin } from '../../plugins/jsDomPlugin';
+import { Component } from '../../component/component';
+import { Ref } from 'core/src/scope/ref';
+
 describe('if directive', () => {
-  test('sample', () => {
-    return;
+  test('simple hiding element', () => {
+    const flag = new Ref<boolean>(true);
+    const rootComponent = new Component({
+      nodes: new RexNode('div', { '[if]': 'flag' }, ['now you see me']),
+      setup() {
+        return { flag };
+      },
+    });
+    const jsDom = new JsDomPlugin('<div id="rexApp" ></div>');
+    new RexApp(rootComponent).extend(jsDom).mount('#rexApp');
+    expect(jsDom.dom.window.document.body.innerHTML).toMatch(
+      /<div id="rexApp"><div __rex__anchor=".*">now you see me<\/div><\/div>/gm,
+    );
+    flag.next(false);
+    expect(jsDom.dom.window.document.body.innerHTML).toMatch(
+      /<div id="rexApp"><\/div>/gm,
+    );
   });
 });

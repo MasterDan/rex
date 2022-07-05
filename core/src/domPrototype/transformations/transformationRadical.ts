21a9b86c0a4a749592e4a0d1d757a16423352db4
MasterDan <MihaylovDS@gmail.com>
2022-07-05 20:48:26 +0300

    add validState

--- core/src/domPrototype/transformations/transformationRadical.ts
+++ core/src/domPrototype/transformations/transformationRadical.ts
@@ -1,25 +1,33 @@
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, combineLatest, filter, Observable } from 'rxjs';
 import { RexNode } from '../rexNode';
 import { DirectiveStructural } from '../../directives/directiveStructural';
 import { DirectiveTransformResult } from '../../directives/directiveBase';
 
 export class TransformationRadical {
-  private _node = new BehaviorSubject<RexNode | null>(null);
-  private _mainDirective = new BehaviorSubject<DirectiveStructural | null>(
+  private _node$ = new BehaviorSubject<RexNode | null>(null);
+  private _mainDirective$ = new BehaviorSubject<DirectiveStructural | null>(
     null,
   );
   size$ = new BehaviorSubject<number>(1);
   positionInParent$ = new BehaviorSubject<number>(0);
 
+  private _validState$: Observable<[DirectiveStructural, RexNode]> =
+    combineLatest([this._mainDirective$, this._node$]).pipe(
+      filter((arr): arr is [DirectiveStructural, RexNode] => {
+        const [dir, node] = arr;
+        return dir != null && node != null;
+      }),
+    );
+
   private _transformedNodes$ = new BehaviorSubject<RexNode[] | null>(null);
 
   setNode(node: RexNode): TransformationRadical {
-    this._node.next(node);
+    this._node$.next(node);
     return this;
   }
 
   setDirective(dir: DirectiveStructural) {
-    this._mainDirective.next(dir);
+    this._mainDirective$.next(dir);
   }
 
   private renderResult(nodes: DirectiveTransformResult): HTMLElement[] {
--- core/src/domPrototype/transformations/transformationSimpe.ts
+++ core/src/domPrototype/transformations/transformationSimpe.ts
@@ -1,19 +1,29 @@
 import { BehaviorMutable } from '../../tools/rx/BehaviorMutable';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, combineLatest, filter, Observable } from 'rxjs';
 import { RexNode } from '../rexNode';
 import { Directive } from '../../directives/directive';
 
 export class TransformationSimple {
-  private _node = new BehaviorSubject<RexNode | null>(null);
-  private _directives = new BehaviorMutable<Directive[] | null>(null);
+  private _node$ = new BehaviorSubject<RexNode | null>(null);
+  private _directives$ = new BehaviorMutable<Directive[] | null>(null);
+
+  private _validState$: Observable<[Directive[], RexNode]> = combineLatest([
+    this._directives$,
+    this._node$,
+  ]).pipe(
+    filter((arr): arr is [Directive[], RexNode] => {
+      const [dirs, node] = arr;
+      return dirs != null && node != null && dirs.length > 0;
+    }),
+  );
 
   setNode(node: RexNode): TransformationSimple {
-    this._node.next(node);
+    this._node$.next(node);
     return this;
   }
 
   pushDirectives(...dirs: Directive[]) {
-    this._directives.mutate((old) => {
+    this._directives$.mutate((old) => {
       const val = old ?? [];
       val.push(...dirs);
       return val;

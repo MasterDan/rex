3a02f4341090058e255f7bd4f0867b344f058e0e
MasterDan <MihaylovDS@gmail.com>
2022-07-16 16:44:49 +0300

    resolving without key

--- core/src/next/di/provide.decorator.ts
+++ core/src/next/di/provide.decorator.ts
@@ -2,13 +2,11 @@ import { Ctor } from 'core/src/tools/types/ctor';
 import { InjectionKey } from './@types/InjectionKey';
 import { diContainer } from './container';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function Provide(key?: InjectionKey) {
-  return function (ctor: Ctor): Ctor {
+  return function (ctor: Ctor) {
     diContainer.register({
       key: key ?? ctor.name,
       ctor,
     });
-    return ctor;
   };
 }
